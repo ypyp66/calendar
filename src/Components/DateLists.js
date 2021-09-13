e634363c32231cc87224be5f5bd8ad348663f9f5
kitebeen <ypyp66@naver.com>
2021-09-14 01:52:50 +0900

    moment 적용하여 dates 불러오기,  레이아웃 및 tag 변경 (#4)

--- src/Components/DateLists.js
+++ src/Components/DateLists.js
@@ -2,23 +2,32 @@ import React from "react";
 import MakeAllDates from "../Utils/MakeAllDates";
 import DateItem from "./DateItem";
 import styled from "styled-components";
+import { TODAY } from "../Constants/Date";
 
 function DateLists({ today }) {
   const allDates = MakeAllDates(today);
-  console.log(allDates);
-  return <Tbody></Tbody>;
+
+  return (
+    <Container>
+      {allDates.map((date) => {
+        console.log(date, today);
+        const selected =
+          date.format("YYYY-M-DD") === today.format(`YYYY-${TODAY.MONTH}-DD`);
+        const otherMonth = date.format("YYYY-MM") !== today.format("YYYY-MM");
+        return (
+          <DateItem selected={selected} otherMonth={otherMonth}>
+            {date.format("D")}
+          </DateItem>
+        );
+      })}
+    </Container>
+  );
 }
 
 export default DateLists;
-
-const Tbody = styled.tbody`
+const Container = styled.article`
   width: 100%;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Tr = styled.tr`
-  text-align: center;
-  margin-top: 10px;
+  height: 90%;
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
 `;
