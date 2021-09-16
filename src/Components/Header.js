56c3f2e5635c1cc68ae08d6e885d6321b95c1b39
kitebeen <ypyp66@naver.com>
2021-09-17 04:28:20 +0900

    콘솔 제거 및 상수 추가

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback } from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 import Button from "Components/CalendarButton";
